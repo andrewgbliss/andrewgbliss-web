68fa28f45ea450751c13077ddc8b647ebe5ce68e
Andrew Bliss <andrewgbliss@gmail.com>
2023-11-05 16:33:56 -0700

    updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,6 @@ import { CommandPrompt } from "./_components/command-prompt";
 import { promises as fs } from "fs";
 
 export default async function Page() {
-  const file = await fs.readFile("./src/app/logo.txt", "utf8");
+  const file = await fs.readFile(process.cwd() + "/lib/logo.txt", "utf8");
   return <CommandPrompt logo={file} />;
 }
