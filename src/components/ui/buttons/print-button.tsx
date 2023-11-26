dba14ff8089b4cdc39aaa3b0f2595f6bebdcb9e2
Andrew Bliss <andrewgbliss@gmail.com>
2023-11-26 16:20:15 -0700

    updates

---
+++ src/components/ui/buttons/print-button.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { Button } from "~/components/ui/button";
+
+export function PrintButton({ onClick }: { onClick: () => void }) {
+  return (
+    <Button size="icon" variant="outline" onClick={onClick}>
+      <IconPrint className="h-4 w-4" />
+      <span className="sr-only">Print</span>
+    </Button>
+  );
+}
+
+function IconPrint(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <polyline points="6 9 6 2 18 2 18 9" />
+      <path d="M6 18H4a2 2 0 0 1-2-2v-5a2 2 0 0 1 2-2h16a2 2 0 0 1 2 2v5a2 2 0 0 1-2 2h-2" />
+      <rect width="12" height="8" x="6" y="14" />
+    </svg>
+  );
+}
--- src/components/ui/cards/qr-card.tsx
+++ src/components/ui/cards/qr-card.tsx
@@ -1,7 +1,16 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
 import QRCode from "react-qr-code";
 import { CardTitle, CardDescription, Card } from "~/components/ui/card";
+import html2canvas from "html2canvas";
+import { PrintButton } from "../buttons/print-button";
 
 export function QrCard({
   title,
@@ -14,8 +23,37 @@ export function QrCard({
   href: string;
   imgSrc: string;
 }) {
+  const saveImg = (el: any) => {
+    void html2canvas(el).then(function (canvas) {
+      simulateDownloadImageClick(canvas.toDataURL(), title + ".png");
+    });
+  };
+
+  function simulateDownloadImageClick(uri: string, filename: string) {
+    const link = document.createElement("a");
+    if (typeof link.download !== "string") {
+      window.open(uri);
+    } else {
+      link.href = uri;
+      link.download = filename;
+      accountForFirefox(clickLink, link);
+    }
+  }
+
+  function clickLink(link: any) {
+    link.click();
+  }
+
+  function accountForFirefox(click: any, link: any) {
+    document.body.appendChild(link);
+    click(link);
+    document.body.removeChild(link);
+  }
   return (
-    <Card className="flex w-full flex-col-reverse divide-x shadow-xl sm:flex-row">
+    <Card
+      id={href}
+      className="flex w-full flex-col-reverse divide-x shadow-xl sm:flex-row"
+    >
       <div className="w-full p-4 sm:w-2/3">
         <Image
           alt={""}
@@ -25,14 +63,19 @@ export function QrCard({
           width="113"
           sizes="(max-width: 768px) 100vw, 33vw"
         />
-        <CardTitle className="text-xl font-bold">
-          <Link href={href} target="_blank">
-            {title}
-          </Link>
-        </CardTitle>
-        <CardDescription className="text-sm text-gray-600 dark:text-gray-400">
-          {tagline}
-        </CardDescription>
+        <div className="flex items-center justify-between">
+          <div>
+            <CardTitle className="text-xl font-bold">
+              <Link href={href} target="_blank">
+                {title}
+              </Link>
+            </CardTitle>
+            <CardDescription className="text-sm text-gray-600 dark:text-gray-400">
+              {tagline}
+            </CardDescription>
+          </div>
+          <PrintButton onClick={() => saveImg(document.getElementById(href))} />
+        </div>
       </div>
       <div className="flex w-full items-center bg-slate-100 p-4 sm:w-1/3">
         <div className="w-full">
