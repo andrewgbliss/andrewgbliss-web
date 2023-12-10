fbe83b683a97d1f3a155a9cbe083ae1bf8c29b01
Andrew Bliss <andrewgbliss@gmail.com>
2023-12-10 11:09:40 -0700

    updates

--- src/components/portfolio/services-section.tsx
+++ src/components/portfolio/services-section.tsx
@@ -1,9 +1,12 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+
 "use client";
 
-/**
- * v0 by Vercel.
- * @see https://v0.dev/t/hAyy5y6yuUb
- */
 import {
   CardTitle,
   CardHeader,
@@ -14,8 +17,15 @@ import {
 import { Badge } from "~/components/ui/badge";
 import { Button } from "~/components/ui/button";
 import Image from "next/image";
-import React from "react";
-import { ScrollSlideUp } from "../ui/animations";
+import React, { useCallback, useEffect, useState } from "react";
+import useEmblaCarousel, { type EmblaOptionsType } from "embla-carousel-react";
+import { NextButton, PrevButton } from "../carousel/carousel";
+
+const options: EmblaOptionsType = {
+  loop: true,
+  slidesToScroll: "auto",
+  containScroll: "trimSnaps",
+};
 
 const ServiceItem = ({
   title,
@@ -29,117 +39,173 @@ const ServiceItem = ({
   badges: React.ReactNode;
 }) => {
   return (
-    <ScrollSlideUp duration={2000} fadeIn>
-      <Card className="flex h-[600px] flex-col justify-between">
-        <CardHeader>
-          <CardTitle className="text-2xl">{title}</CardTitle>
-        </CardHeader>
-        <div className="mx-auto h-64 w-64 overflow-hidden">
-          <Image
-            alt={title}
-            className="w-full object-cover"
-            width="200"
-            height="300"
-            src={img}
-            sizes="(max-width: 768px) 100vw, 100vw"
-          />
-        </div>
-        <CardContent className="space-y-4 py-2">
-          <p className="text-lg">{description}</p>
-          <div>{badges}</div>
-        </CardContent>
-        <CardFooter className="pt-5">
-          <Button variant={"action"}>Contact Us</Button>
-        </CardFooter>
-      </Card>
-    </ScrollSlideUp>
+    <Card className="flex flex-col justify-between">
+      <CardHeader>
+        <CardTitle className="text-2xl">{title}</CardTitle>
+      </CardHeader>
+      <div className="mx-auto h-64 w-64 overflow-hidden">
+        <Image
+          alt={title}
+          className="w-full object-cover"
+          width="200"
+          height="300"
+          src={img}
+          sizes="(max-width: 768px) 100vw, 100vw"
+          priority
+        />
+      </div>
+      <CardContent className="space-y-4 py-2">
+        <p className="text-lg">{description}</p>
+        <div>{badges}</div>
+      </CardContent>
+      <CardFooter className="pt-5">
+        <Button variant={"action"}>Contact Us</Button>
+      </CardFooter>
+    </Card>
   );
 };
 
+const serviceItems = [
+  {
+    title: "Web Design",
+    description:
+      "We provide modern and responsive web design services for your business.",
+    img: "/img/web-develop.webp",
+    badges: [
+      { className: "border-blue-500m mr-2 text-blue-500", text: "HTML" },
+      { className: "mr-2 border-green-500 text-green-500", text: "CSS" },
+      { className: "border-pink-500 text-pink-500", text: "JavaScript" },
+    ],
+  },
+  {
+    title: "Web Development",
+    description:
+      "Our team offers comprehensive web development services using the latest technologies.",
+    img: "/img/soft-develop.webp",
+    badges: [
+      { className: "mr-2 border-yellow-500 text-yellow-500", text: "React" },
+      { className: "mr-2 border-blue-500 text-blue-500", text: "Next.js" },
+      { className: "border-green-500 text-green-500", text: "PostgreSQL" },
+    ],
+  },
+  {
+    title: "UI / UX Development",
+    description:
+      "User interface design software, wireframes, user flow diagrams, sketchbooks, color swatches, and a creative, inspirational setting.",
+    img: "/img/uiux-develop.webp",
+    badges: [
+      { className: "mr-2 border-red-500 text-red-500", text: "Tailwind" },
+      { className: "border-blue-500 text-blue-500", text: "Image Editing" },
+    ],
+  },
+  {
+    title: "Web Design",
+    description:
+      "We provide modern and responsive web design services for your business.",
+    img: "/img/web-develop.webp",
+    badges: [
+      { className: "border-blue-500m mr-2 text-blue-500", text: "HTML" },
+      { className: "mr-2 border-green-500 text-green-500", text: "CSS" },
+      { className: "border-pink-500 text-pink-500", text: "JavaScript" },
+    ],
+  },
+  {
+    title: "Web Development",
+    description:
+      "Our team offers comprehensive web development services using the latest technologies.",
+    img: "/img/soft-develop.webp",
+    badges: [
+      { className: "mr-2 border-yellow-500 text-yellow-500", text: "React" },
+      { className: "mr-2 border-blue-500 text-blue-500", text: "Next.js" },
+      { className: "border-green-500 text-green-500", text: "PostgreSQL" },
+    ],
+  },
+  {
+    title: "UI / UX Development",
+    description:
+      "User interface design software, wireframes, user flow diagrams, sketchbooks, color swatches, and a creative, inspirational setting.",
+    img: "/img/uiux-develop.webp",
+    badges: [
+      { className: "mr-2 border-red-500 text-red-500", text: "Tailwind" },
+      { className: "border-blue-500 text-blue-500", text: "Image Editing" },
+    ],
+  },
+];
+
 export function ServicesSection() {
+  const [emblaRef, emblaApi] = useEmblaCarousel(options);
+
+  const [prevBtnDisabled, setPrevBtnDisabled] = useState(true);
+  const [nextBtnDisabled, setNextBtnDisabled] = useState(true);
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const [scrollSnaps, setScrollSnaps] = useState([]);
+
+  const scrollPrev = useCallback(
+    () => emblaApi && emblaApi.scrollPrev(),
+    [emblaApi],
+  );
+  const scrollNext = useCallback(
+    () => emblaApi && emblaApi.scrollNext(),
+    [emblaApi],
+  );
+
+  const onInit = useCallback((emblaApi: any) => {
+    setScrollSnaps(emblaApi.scrollSnapList());
+  }, []);
+
+  const onSelect = useCallback((emblaApi: any) => {
+    setSelectedIndex(emblaApi.selectedScrollSnap());
+    setPrevBtnDisabled(!emblaApi.canScrollPrev());
+    setNextBtnDisabled(!emblaApi.canScrollNext());
+  }, []);
+
+  useEffect(() => {
+    if (!emblaApi) return;
+
+    onInit(emblaApi);
+    onSelect(emblaApi);
+    emblaApi.on("reInit", onInit);
+    emblaApi.on("reInit", onSelect);
+    emblaApi.on("select", onSelect);
+  }, [emblaApi, onInit, onSelect]);
+
   return (
-    <section className="min-h-screen bg-gradient-to-r from-gray-50 to-gray-100 py-12 pb-12  text-black dark:from-zinc-900 dark:to-zinc-800 dark:text-white">
+    <section className="min-h-screen bg-gradient-to-r from-gray-50 to-gray-100 py-12 pb-24  text-black dark:from-zinc-900 dark:to-zinc-800 dark:text-white">
       <div className="text-center">
         <h2 className="text-5xl font-bold">Services</h2>
       </div>
-      <div className="container mx-auto">
-        <div className="grid grid-cols-1 gap-6 p-6 md:grid-cols-2 lg:grid-cols-3">
-          <ServiceItem
-            title="Web Design"
-            description="We provide modern and responsive web design services for your
-          business."
-            img="/img/web-develop.webp"
-            badges={
-              <>
-                <Badge
-                  className="border-blue-500m mr-2 text-blue-500"
-                  variant="outline"
-                >
-                  HTML
-                </Badge>
-                <Badge
-                  className="mr-2 border-green-500 text-green-500"
-                  variant="outline"
-                >
-                  CSS
-                </Badge>
-                <Badge
-                  className="border-pink-500 text-pink-500"
-                  variant="outline"
-                >
-                  JavaScript
-                </Badge>
-              </>
-            }
-          />
-          <ServiceItem
-            title="Web Development"
-            description="Our team offers comprehensive web development services using the latest technologies."
-            img="/img/soft-develop.webp"
-            badges={
-              <>
-                <Badge
-                  className="mr-2 border-yellow-500 text-yellow-500"
-                  variant="outline"
-                >
-                  React
-                </Badge>
-                <Badge
-                  className="mr-2 border-blue-500 text-blue-500"
-                  variant="outline"
-                >
-                  Next.js
-                </Badge>
-                <Badge
-                  className="border-green-500 text-green-500"
-                  variant="outline"
-                >
-                  PostgreSQL
-                </Badge>
-              </>
-            }
-          />
-          <ServiceItem
-            title="UI / UX Development"
-            description=" User interface design software, wireframes, user flow diagrams, sketchbooks, color swatches, and a creative, inspirational setting."
-            img="/img/uiux-develop.webp"
-            badges={
-              <>
-                <Badge
-                  className="mr-2 border-red-500 text-red-500"
-                  variant="outline"
-                >
-                  Tailwind
-                </Badge>
-                <Badge
-                  className="border-blue-500 text-blue-500"
-                  variant="outline"
-                >
-                  Image Editing
-                </Badge>
-              </>
-            }
-          />
+      <div className="container relative mx-auto p-5">
+        <div className={`embla__services_full sm:embla__services_half`}>
+          <div className={`embla__services__viewport`} ref={emblaRef}>
+            <div className={`embla__services__container`}>
+              {serviceItems.map((item, i) => (
+                <div className="embla__services__slide" key={i}>
+                  <ServiceItem
+                    title={item.title}
+                    description={item.description}
+                    img={item.img}
+                    badges={
+                      <>
+                        {item.badges.map((badge) => (
+                          <Badge
+                            key={badge.text}
+                            className={badge.className}
+                            variant="outline"
+                          >
+                            {badge.text}
+                          </Badge>
+                        ))}
+                      </>
+                    }
+                  />
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+        <div className="embla__buttons">
+          <PrevButton onClick={scrollPrev} disabled={prevBtnDisabled} />
+          <NextButton onClick={scrollNext} disabled={nextBtnDisabled} />
         </div>
       </div>
     </section>
